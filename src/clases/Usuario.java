48fe0bacf27494803558895e4c66e766cc50549c
Ciprian Chis <ciprianchisalin@gmail.com>
2020-04-16 14:26:07 +0200

    Inicio

---
+++ src/clases/Usuario.java
@@ -0,0 +1,49 @@
+package clases;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Usuario implements Serializable{
+	private String nombreUsuario;
+	private String password;
+	private boolean esAdmin;
+	private ArrayList<Reserva> vReserva;
+
+	public Usuario(String nombreUsuario, String password, boolean esAdmin) {
+		super();
+		this.nombreUsuario = nombreUsuario;
+		this.password = password;
+		this.esAdmin = esAdmin;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public boolean isEsAdmin() {
+		return esAdmin;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setEsAdmin(boolean esAdmin) {
+		this.esAdmin = esAdmin;
+	}
+
+	@Override
+	public String toString() {
+		return "\n"+ nombreUsuario + "-" + password + "-" + esAdmin;
+	}
+	
+
+}
